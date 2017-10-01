d3b11c6b6337f8ab22c2d11de1901dfbd67e4869
Bailey Bryant <bailey.bryant10@gmail.com>
2017-10-01 15:59:10 -0500

    clean up notes, add reame

--- src/actionTypes/actions.js
+++ src/actionTypes/actions.js
@@ -1,11 +1,5 @@
 export const FILTER_PRODUCTS = 'FILTER_PRODUCTS';
 
-// export const filterProducts = function (filterType) {
-//   return {
-//     type: FILTER_PRODUCTS,
-//     payload: filterType
-//   }
-// }
 
 const makeActionCreator = function (actionType) {
   return function (payload) {
--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -18,21 +18,10 @@ import Product from "../components/Product";
     );
   }
 }
-        // Create a dynamically populated list of `<Product />` components
-        // Each `<Product />` component should have a single object from the `products` state property (array)
-        // applied to the component as a `product` property
 
-
-
-
-// Using the `mapStateToProps` function, filter the array stored in the
-// state `products` property based on 3 criterea:
-// - `underTwenty`
-// - `overTwenty`
-// - `all` or the default
 const mapStateToProps = function(state) {
     let products;
-    // complete the `if else` statement including conditions and `products` value
+  
     if (state.filter === "underTwenty") {
       products = state.products.filter(p => p.price<20)
     } else if ( state.filter ==="overTwenty") {
